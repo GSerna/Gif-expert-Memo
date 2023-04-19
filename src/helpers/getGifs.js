2c20d12636d7d3a6aea2ea4dda159cbc9eef6a03
Guillermo Serna <34690932+GSerna@users.noreply.github.com>
2023-04-18 23:02:41 -0600

    probar api key

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,5 +1,6 @@
 export const getGifs = async( category ) => {
 
+    /*const url = `https://api.giphy.com/v1/gifs/search?api_key=FLcOhMIFJpl9haPJOORtq2mgLawtZusf&q=${ category }&limit=25&offset=0&rating=g&lang=en`;*/
     const url = `https://api.giphy.com/v1/gifs/search?api_key=FLcOhMIFJpl9haPJOORtq2mgLawtZusf&q=${ category }&limit=25&offset=0&rating=g&lang=en`;
     const resp = await fetch( url );
     const { data } = await resp.json();
