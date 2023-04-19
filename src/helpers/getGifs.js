3e0a5e7ee8d85d520abb142f0477f159490c362b
Guillermo Serna <34690932+GSerna@users.noreply.github.com>
2023-04-18 22:49:19 -0600

    se actualiza apikey

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,6 +1,6 @@
 export const getGifs = async( category ) => {
 
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=kaJ1JwD4CuQgYun7YpTQpTr5p1qs1sQn&q=${ category }&limit=10`;
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=X3DdzjHI1LO83eU3Ie87tD8D6PzjCtaZ&q=${ category }&limit=10`;
     const resp = await fetch( url );
     const { data } = await resp.json();
 
